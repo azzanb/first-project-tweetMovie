6ca327aa1c04b71775867e1a57cfdb99fc31739d
Azzan Braxton <azzanbraxton@gmail.com>
2018-02-04 12:06:21 -0500

    config added

---
+++ src/routes/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'mvusgmDhTQ2mqGoMXFB4jxRUI',
+  consumer_secret:      'q18Uxb6RIkKiJMz030KTPpFbv9v0bCdn94d43vu58Oi2osU1sN',
+  access_token:         '897057509990694912-j9eeCpZag7HxYPnqMuhXdEccFGzDPzn',
+  access_token_secret:  'N9QEaLicuwaOq4Y70LqjLV98ovpLcq4LYIxkieiKkKOlu'
+};
+
