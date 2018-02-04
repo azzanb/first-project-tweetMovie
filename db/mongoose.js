ab16a0d6a8dc40c25ca70b3073fec453602e9c37
Azzan Braxton <azzanbraxton@gmail.com>
2018-02-04 09:42:54 -0500

    MongoDB URI fixed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,8 @@
 var mongoose = require("mongoose");
-var MongoClient = require('mongodb').MongoClient;
 
 //Use promises
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/UserApp");
+mongoose.connect(process.env.MONGOLAB_CRIMSON_URI || "mongodb://localhost:27017/UserApp");
 
 module.exports = {mongoose};
