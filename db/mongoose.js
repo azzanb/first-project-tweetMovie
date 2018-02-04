a17dee77209b88756d9eb2ea6e840d8a7b64ea41
Azzan Braxton <azzanbraxton@gmail.com>
2018-02-04 13:03:03 -0500

    MongoDB URI Changed

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require("mongoose");
 //Use promises
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/UserApp");
+mongoose.connect(process.env.MONGOLAB_MAUVE_URI || "mongodb://localhost:27017/UserApp");
 
 module.exports = {mongoose};
